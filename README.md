42b4145f3682c620d61e9547a3c1fd2e8164262d
felipez <tifonzafel@gmail.com>
2021-04-15 22:06:28 +0200

    add README

---
+++ README.md
@@ -0,0 +1,5 @@
+[![build](https://github.com/NLESC-JCER/carbon/actions/workflows/rust.yml/badge.svg)](https://github.com/NLESC-JCER/carbon/actions/workflows/rust.yml)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+# Carbon
+A tiny Rust library used for didactical purposes.
--- src/monte_carlo.rs
+++ src/monte_carlo.rs
@@ -1,16 +1,17 @@
 extern crate ndarray_rand;
-use ndarray::prelude::*;
+use ndarray::prelude::*; // Import the most used types, functions, etc.
 use ndarray_rand::rand_distr::Uniform;
 use ndarray_rand::RandomExt;
 
 /// Compute the PI number using the Monte-Carlo method
 /// * `nsamples` - number of samples to use
 pub fn compute_pi(nsamples: usize) -> f64 {
+    // Code comments on the right are the equivalent Python (Numpy) operations
     let xs = Array1::random(nsamples, Uniform::new(0., 1.)); // xs = np.random.uniform(size=samples)
     let ys = Array1::random(nsamples, Uniform::new(0., 1.)); // ys = np.random.uniform(size=samples)
-    let xs = xs.mapv(|x| f64::powi(x, 2)); // xs ** 2
-    let ys = ys.mapv(|y| f64::powi(y, 2)); // ys ** 2
-    let rs = (xs + ys).mapv(f64::sqrt); // np.sqrt( xs + ys)
-    let sum = rs.fold(0.0, |acc, &r| if r < 1.0 { acc + 1. } else { acc });
+    let xs = xs.mapv(|x| f64::powi(x, 2)); // xs =  xs ** 2
+    let ys = ys.mapv(|y| f64::powi(y, 2)); // ys = ys ** 2
+    let rs = (xs + ys).mapv(f64::sqrt); // rs = np.sqrt( xs + ys)
+    let sum = rs.fold(0.0, |acc, &r| if r < 1.0 { acc + 1. } else { acc }); // rs = rs[rs > 1.0]; sum = float(len(rs))
     sum * 4.0 / (nsamples as f64)
 }
